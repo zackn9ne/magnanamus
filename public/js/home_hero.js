b18d250630792c4c3a43c7c680d55c6e890c3145
netkam <netkam@netkams-MBP.home>
2016-08-31 17:35:17 -0400

    Gruntfile handling JS&CSS deps and minifiying plus homepage edits checked in WED

---
+++ public/js/home_hero.js
@@ -0,0 +1,19 @@
+$(function() {
+	var stretchedImg = 'img/Screen-Shot-2016-07-26-at-9.41.50-PM-1110x600.png';
+	var stretchedEl = $(".container.hero");
+
+		stretchedEl.css("min-height", "300px");
+
+		function loadHeroBackstretchIt()
+		{
+
+		if(window.devicePixelRatio >= 2) {
+			$(".container.hero").backstretch(stretchedImg);
+			$("img");
+		} else {
+			$(".container.hero").backstretch(stretchedImg);
+			}
+		}
+
+		loadHeroBackstretchIt();
+})
