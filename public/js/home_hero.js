b3a2eafbbe5ed8d323720afb2703b6971ce5642d
netkam <netkam@netkams-MBP.home>
2016-09-21 21:00:34 -0400

    refactor scss

--- public/js/home_hero.js
+++ public/js/home_hero.js
@@ -2,7 +2,7 @@ $(function() {
 	var stretchedImg = 'img/Screen-Shot-2016-07-26-at-9.41.50-PM-1110x600.png';
 	var stretchedEl = $(".container.hero");
 
-		stretchedEl.css("min-height", "300px");
+		stretchedEl.css("min-height", "360px");
 
 		function loadHeroBackstretchIt()
 		{
