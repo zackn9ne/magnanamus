68df7df056a18aac7f50f5874a07826077f2044e
zackn9ne <zackn9ne@gmail.com>
2016-09-28 16:23:26 -0400

    just checking a midship build from a usb

--- model/db.js
+++ model/db.js
@@ -1,2 +1,3 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/sandbox0');
+// mongoose.connect('mongodb://localhost/sandbox0');
+mongoose.connect('mongodb://zackn9ne:pass1word@ds041586.mlab.com:41586/mlab_stowage')
